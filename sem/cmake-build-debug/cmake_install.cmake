c3d2911a747e2a0843bcfa6b2a4626fbaef8401a
ZYGAL0 <efimov.se@phystech.edu>
2019-11-06 21:48:33 +0300

    Pipe with sem

--- sem/cmake-build-debug/cmake_install.cmake
+++ sem/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/zygalo/C/sem
+# Install script for directory: /home/zygalo/C/Mipt_3_sem/sem
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/zygalo/C/sem/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/zygalo/C/Mipt_3_sem/sem/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
