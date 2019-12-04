3286014430ac73722c1e2d4bd612427fba12b278
ZYGAL0 <efimov.se@phystech.edu>
2019-12-05 00:23:39 +0300

    Sorting by TCP

---
+++ TCP/cmake-build-debug/CMakeFiles/TCP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TCP.dir/server.c.o"
+  "TCP"
+  "TCP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TCP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
